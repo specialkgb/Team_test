120379655ef309ec545e98f63d0cec472023512d
specialkgb <specialkgb@naver.com>
2020-08-10 15:08:52 +0900

    Book_CRUD_Start

---
+++ src/main/java/com/biz/team/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.biz.team.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+	
+	public String list() {
+		return "book/list";
+		
+	}
+
+}
